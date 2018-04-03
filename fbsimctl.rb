3e79dc7788d73cf0d75ed8126e16b0e69236f4de
Florian Wittmann <git@florianwittmann.de>
2018-04-03 16:42:51 +0200

    Create fbsimctl.rb

---
+++ fbsimctl.rb
@@ -0,0 +1,18 @@
+class Fbsimctl < Formula
+  desc "A Powerful Command Line for Managing iOS Simulators"
+  homepage "https://github.com/facebook/FBSimulatorControl/fbsimctl/README.md"
+  url "https://github.com/facebook/FBSimulatorControl/tarball/v0.4.0"
+  sha256 "4a8bb8aed15f756aeb57e4a1f8724e40a3dfaa27740ae1644021386ab64bd9fb"
+  head "https://github.com/florianwittmann/FBSimulatorControl.git"
+
+  depends_on "carthage"
+  depends_on :xcode => ["8.2", :build]
+
+  def install
+    system "./build.sh", "fbsimctl", "build", prefix
+  end
+
+  test do
+    system "fbsimctl", "list"
+  end
+end
